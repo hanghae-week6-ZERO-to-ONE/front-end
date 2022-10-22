db4fd1901f302c6b34cbfc44ee1e4eef2d2e21f0
kyunghoonkook <rnrrudgg68@gmail.com>
2022-10-23 00:17:43 +0900

    헤더 수정

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,28 +1,16 @@
 import Layout from "../../components/Layout";
 import styled from "styled-components";
 import Background from "../../components/Background";
-import DrinkList from "../../features/Home/DrinkList";
-import RecentlyList from "../../features/Home/RecentList";
+import DrinkListWrap from "../../features/Home/DrinkListWrap";
 
 const Home = () => {
 	return (
 		<>
 			<Background />
 			<Layout>
-				<DrinkListWrap>
-					<RecentlyList />
-					<DrinkList />
-					<DrinkList />
-					<DrinkList />
-					<DrinkList />
-				</DrinkListWrap>
+				<DrinkListWrap />
 			</Layout>
 		</>
 	);
 };
 export default Home;
-
-const DrinkListWrap = styled.section`
-	width: 1280px;
-	margin: 120px auto;
-`;
