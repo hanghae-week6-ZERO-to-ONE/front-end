df59512d9cc1bfb53031ea2eadc563c2f9a146df
cho_jae_sin <1005jsc@naver.com>
2022-10-22 09:08:52 +0900

    prettierrc added

--- src/shared/PrivateRoute.jsx
+++ src/shared/PrivateRoute.jsx
@@ -3,7 +3,7 @@ import { Navigate, Outlet } from 'react-router-dom';
 
 const PrivateRoute = ({ login, ...rest }) => {
   if (!login) {
-    return <Navigate to='/home/' replace={true} />;
+    return <Navigate to="/home/" replace={true} />;
   }
 
   return <Outlet context={{ ...rest }} />;
