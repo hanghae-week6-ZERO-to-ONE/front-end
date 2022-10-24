2e50ba0dd165e0c373a2c4527661ca6272e41f79
kyunghoonkook <rnrrudgg68@gmail.com>
2022-10-24 10:31:19 +0900

    대기

--- src/components/my_page/UserContent.jsx
+++ src/components/my_page/UserContent.jsx
@@ -2,14 +2,14 @@ import UserContentCmt from "../my_page/UserContentCmt";
 import UserContentImg from "../my_page/UserContentImg";
 // import SettingUserThumbnail from "./OnImgChange";
 
-// const UserInfo = () => {
-// 	return (
-// 		<>
-// 			<UserContentCmt />
-// 			{/* <SettingUserThumbnail /> */}
-// 			<UserContentImg />
-// 		</>
-// 	);
-// };
+const UserInfo = () => {
+	return (
+		<>
+			<UserContentCmt />
+			{/* <SettingUserThumbnail /> */}
+			<UserContentImg />
+		</>
+	);
+};
 
-// export default UserInfo;
+export default UserInfo;
--- src/components/my_page/UserInfo.jsx
+++ src/components/my_page/UserInfo.jsx
@@ -1,13 +1,13 @@
 import UserInfoImg from "../my_page/UserInfoImg";
 import UserInfoPw from "../my_page/UserInfoPw";
 
-// const UserInfo = () => {
-// 	return (
-// 		<>
-// 			<UserInfoImg />
-// 			<UserInfoPw />
-// 		</>
-// 	);
-// };
+const UserInfo = () => {
+	return (
+		<>
+			<UserInfoImg />
+			<UserInfoPw />
+		</>
+	);
+};
 
-// export default UserInfo;
+export default UserInfo;
